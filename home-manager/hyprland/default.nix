727e7b5eafb2bf1ac6ce29f6b342dddf19029095
Nicolas <mueller@cryptospace.dev>
2023-10-12 19:32:44 +0200

    hyprland keymap tries

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -5,11 +5,16 @@
     wayland.windowManager.hyprland.extraConfig =
         builtins.readFile ./general.conf +
         builtins.readFile ./windowrules.conf +
+        builtins.readFile ./workspacerules.conf +
         builtins.readFile ./keybinds.conf;
+
+    # Link custom XKB file
+    xdg.configFile."xkb/symbols/us-german".source = ./us-german.xkb;
 
     # Packages needed on Hyprland specifically, in addition to a standard desktop
     home.packages = with pkgs; [
         gnome.nautilus
         kitty
+        ulauncher
     ];
 }
