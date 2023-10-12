0278863e692944c635646b6007d311f48f4f1171
Nicolas <mueller@cryptospace.dev>
2023-10-12 19:48:24 +0200

    kitty configuration through home manager

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -3,18 +3,27 @@
     # Basic Hyprland configuration
     wayland.windowManager.hyprland.enable = true;
     wayland.windowManager.hyprland.extraConfig =
-        builtins.readFile ./general.conf +
-        builtins.readFile ./windowrules.conf +
-        builtins.readFile ./workspacerules.conf +
-        builtins.readFile ./keybinds.conf;
+        builtins.readFile ./hypr/general.conf +
+        builtins.readFile ./hypr/windowrules.conf +
+        builtins.readFile ./hypr/workspacerules.conf +
+        builtins.readFile ./hypr/keybinds.conf;
 
     # Link custom XKB file
-    xdg.configFile."xkb/symbols/us-german".source = ./us-german.xkb;
+    xdg.configFile."xkb/symbols/us-german".source = ./hypr/us-german.xkb;
+
+    programs.kitty = {
+        enable = true;
+        settings = {
+            show_hyperlink_targets = true;
+            copy_on_select = "clipboard";
+            strip_trailing_spaces = "smart";
+            focus_follows_mouse = true;
+        };
+    };
 
     # Packages needed on Hyprland specifically, in addition to a standard desktop
     home.packages = with pkgs; [
         gnome.nautilus
-        kitty
         ulauncher
     ];
 }
