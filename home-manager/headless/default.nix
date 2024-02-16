110904d71e2726edfac2539a328f77792557d9e6
Nico Müller <mueller@cryptospace.dev>
2024-02-16 19:34:59 +0100

    📨 git config with home manager

--- home-manager/headless/default.nix
+++ home-manager/headless/default.nix
@@ -1,71 +1,89 @@
-{ config, pkgs, lib, username, ... }:
+{ pkgs, username, ... }:
 {
-    # Basic home manager settings
-    programs.home-manager.enable = true;
+  # Basic home manager settings
+  programs.home-manager.enable = true;
 
-    home.username = username;
-    home.homeDirectory = "/home/${username}";
-    home.stateVersion = "23.05";
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
+  home.stateVersion = "23.05";
 
-    home.packages = with pkgs; [
-	# Ensure essential programs are installed
-	gcc
-    	
-        # Terminal programs
-        fzf # Lifesaver!
-        autojump # Lifesaver #2!
-        tldr
-        bat
-        lsd
+  home.packages = with pkgs; [
+    # Ensure essential programs are installed
+    gcc
 
-        typescript
-        sassc
-    ];
+    # Terminal programs
+    fzf # Lifesaver!
+    autojump # Lifesaver #2!
+    tldr
+    bat
+    lsd
 
-    imports = [ 
-        ./tmux.nix
-        ./neovim.nix
-        ../theme/default.nix
-    ];
+    typescript
+    sassc
+  ];
 
-    programs = {
-        zsh = {
-            enable = true;
-            defaultKeymap = null;
-            autocd = true;
+  imports = [
+    ./tmux.nix
+    ./neovim.nix
+    ../theme/default.nix
+  ];
 
-            # Plugins
-            enableAutosuggestions = true;
-            enableCompletion = true;
-            syntaxHighlighting.enable = true;
+  programs = {
+    zsh = {
+      enable = true;
+      defaultKeymap = null;
+      autocd = true;
 
-            initExtra = builtins.readFile ../../config/zshrc;
+      # Plugins
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
 
-            oh-my-zsh = {
-                enable = true;
-                theme = "skaro";
-                plugins = [
-                    "autojump"
-                    "colorize"
-                ];
-            };
+      initExtra = builtins.readFile ../../config/zshrc;
 
-            # Aliases
-            shellAliases = {
-                "cat" = "bat";
-                "ls" = "lsd";
-                "la" = "lsd -lah";
-                "virsh" = "virsh -c qemu:///system";
-                "fzf" = "fzf --reverse";
-            };
-        };
+      oh-my-zsh = {
+        enable = true;
+        theme = "skaro";
+        plugins = [
+          "autojump"
+          "colorize"
+        ];
+      };
 
-        btop = {
-            enable = true;
-            settings = {
-                vim_keys = true;
-            };
-        };
+      # Aliases
+      shellAliases = {
+        "cat" = "bat";
+        "ls" = "lsd";
+        "la" = "lsd -lah";
+        "virsh" = "virsh -c qemu:///system";
+        "fzf" = "fzf --reverse";
+      };
     };
-    
+
+    btop = {
+      enable = true;
+      settings = {
+        vim_keys = true;
+      };
+    };
+
+    git = {
+      enable = true;
+      lfs.enable = true;
+      userEmail = "mueller@cryptospace.dev";
+      userName = "Nico Müller";
+      signing = {
+        signByDefault = true;
+        key = "~/.ssh/id_rsa_github.pub";
+      };
+      extraConfig = {
+        init = {
+          defaultBranch = "main";
+        };
+        gpg = {
+          format = "ssh";
+        };
+      };
+    };
+  };
 }
