08783162c51bc6022762f185d206f3f3e7ea002c
Nico Müller <mueller@cryptospace.dev>
2024-11-02 11:47:53 +0100

    fixed bug in applauncher selecting all processes containing "rofi" instead of the exact match

--- pkgs/scripts/applauncher.sh
+++ pkgs/scripts/applauncher.sh
@@ -4,7 +4,7 @@
 ACTIVE_WINDOW_CLASS=$(hyprctl activewindow -j | jq -r .class)
 
 if [ $ACTIVE_WINDOW_CLASS != "looking-glass-client" ]; then
-    rofi=$(pgrep rofi)
+    rofi=$(pgrep -x rofi)
 
     if [ -z "$rofi" ]; then
         rofi -show combi
