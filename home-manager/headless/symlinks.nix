8ef3a653f6138681ec4eec64d74cc6f98c94020e
Nico Müller <mueller@cryptospace.dev>
2025-07-03 18:29:23 +0200

    update

--- home-manager/headless/symlinks.nix
+++ home-manager/headless/symlinks.nix
@@ -2,6 +2,7 @@
 {
   home.file.".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/config/zshrc";
   home.file.".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/config/tmux.conf";
+  home.file.".ideavimrc".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/config/ideavimrc";
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/config/nvim";
   xdg.configFile."mimeapps.list".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/config/mimeapps.list";
   xdg.configFile."brave-flags.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/config/brave-flags.conf";
